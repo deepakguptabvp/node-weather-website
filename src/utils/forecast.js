929ae512b62092ff4fe1c4587b86064175ab80df
unknown <you@example.com>
2021-04-23 20:02:51 +0530

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,18 @@
 const request = require('postman-request')
 
+
+
 const forecast = (latitude, longitude , callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=f31499b1ed15f562116550d1c9bb1e3f&query='+ latitude + ',' + longitude + '&units=f'
 
 request({ url, json: true }, (error, {body}) => {
-    if(error) {
-        callback('unable to connect to forecast-app', undefined )
+    if (error) {
+        callback('unable to connect to forecast-app !', undefined )
     } else if (body.error) {
        callback('location not found.' , undefined)
     } else {
-        callback( undefined,'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degree temperature . There is a ' + body.current.precip + ' % chance of rain.') 
+        
+        callback( undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degree out . The humidty is '+ body.current.humidity  +' %. There is a ' + body.current.precip + ' % chance of rain.') 
     } 
 })
 }
